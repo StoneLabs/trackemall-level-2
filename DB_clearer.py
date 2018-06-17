f8a2f7038ca7ba63b091a81aa6517e99dc6c1014
StoneInc <levyehrstein@googlemail.com>
2018-06-17 10:17:19 +0200

    Aded db clearing script

---
+++ DB_clearer.py
@@ -0,0 +1,4 @@
+# Clear frames bucket
+for _id in range(0, 1000):
+    cmd = ['wget', "127.0.0.1:2438/getFrame?id=" + str(_id), "-O", "/tmp/next_frame.dat", "wb+"]
+    prc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input="")
