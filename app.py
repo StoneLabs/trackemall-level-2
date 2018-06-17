a420e56fa9376b0359cba6cc1578aecfebd18c2c
PeterHeynmoeller <peddyheynmoeller@gmail.com>
2018-06-17 22:19:13 +0200

    mache ich morgen, ist aber schonmal ein bisschen besser

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def getDetection():
     dirc = os.listdir("/tmp/n/")
     if(len(dirc)):
         id = + str(request.args.get("id"))
-        f = open("/tmp/n/" + id, "rw")
+        f = open("/tmp/n/" + id, "r")
         #os.remove("/tmp/n/" + id) ONLY FOR LIVE
         return f
     return "-1"
@@ -21,9 +21,9 @@ def getDetection():
 @app.route("/getFrame", methods=['GET'])
 def getFrame():
     dirc = os.listdir("/tmp/o/")
-    if(len(objects)):
-        id = + str(request.args.get("id"))
-        f = open("/tmp/o/" + id, "rw")
+    if(len(dirc)):
+        id = str(request.args.get("id"))
+        f = open("/tmp/o/" + id, "r")
         os.remove("/tmp/o/" + id)
         return f
     return "-1"
@@ -31,23 +31,28 @@ def getFrame():
 @app.route("/getNext", methods=['GET'])
 def getNext():
     dirc = os.listdir("/tmp/o/")
-    if(len(dir)):
-        return str(min(dir))
+    if(len(dirc)):
+        return str(min(dirc))
     return "-1"
 
 @app.route("/setDetection", methods=['POST'])
 def setDetection():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/n/'+str(counter))
+    f.save('/tmp/n/'+ str(counter))
     return "top"
 
 @app.route("/addFrame", methods=['POST'])
 def addFrame():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/o/'+str(counter))
+    f.save('/tmp/o/'+ str(counter))
     return "top"
 
 if __name__ == '__main__':
+    try:
+        os.mkdir("/tmp/n/", 777)
+        os.mkdir("/tmp/o/", 777)
+    except:
+        print("already there")
     app.run(host='0.0.0.0', port=2438, debug=True)
