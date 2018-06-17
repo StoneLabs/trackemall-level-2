fcefd60385e9f72e117cdca2a36230a78938d767
PeterHeynmoeller <peddyheynmoeller@gmail.com>
2018-06-17 18:00:06 +0200

    removed minio

--- app.py
+++ app.py
@@ -4,71 +4,50 @@
 import os
 from flask import Flask, request
 from flask_cors import CORS
-from minio import Minio
-
 app = Flask(__name__)
 
 CORS(app)
 
-mc = Minio("minio:9000", access_key="belastend", secret_key="belastend42", secure=False)
-
-try:
-    mc.make_bucket("frames")
-    mc.make_bucket("fframes")
-except:
-    print("fuck")
-
 @app.route("/getDetection", methods=['GET'])
 def getDetection():
-    objects = list(mc.list_objects_v2('fframes'))
-    if(len(objects)):
-        x = b''
-        z = str(request.args.get("id"))
-        y = mc.get_object('fframes', z).stream(32*1024)
-        for d in y:
-            x += d
-        #mc.remove_object('fframes', z)
-        return x
-    return str(-1)
+    dirc = os.listdir("/tmp/n/")
+    if(len(dirc)):
+        id = + str(request.args.get("id"))
+        f = open("/tmp/n/" + id, "rw")
+        #os.remove("/tmp/n/" + id) ONLY FOR LIVE
+        return f
+    return "-1"
 
 @app.route("/getFrame", methods=['GET'])
 def getFrame():
-    objects = list(mc.list_objects_v2('frames'))
+    dirc = os.listdir("/tmp/o/")
     if(len(objects)):
-        x = b''
-        z = str(request.args.get("id"))
-        y = mc.get_object('frames', z).stream(32*1024)
-        for d in y:
-            x += d
-        mc.remove_object('frames', z)
-        return x
-    return str(-1)
+        id = + str(request.args.get("id"))
+        f = open("/tmp/o/" + id, "rw")
+        os.remove("/tmp/o/" + id)
+        return f
+    return "-1"
 
 @app.route("/getNext", methods=['GET'])
 def getNext():
-    objects = list(mc.list_objects_v2('frames'))
-    if(len(objects)):
-        z = str(min(objects,key= lambda x: int(x.object_name.encode('utf-8'))).object_name)
-        return z
-    return str(-1)
+    dirc = os.listdir("/tmp/o/")
+    if(len(dir)):
+        return str(min(dir))
+    return "-1"
 
 @app.route("/setDetection", methods=['POST'])
 def setDetection():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/n'+str(counter))
-    mc.fput_object("fframes", str(counter), "/tmp/n"+str(counter), "text/plain")
-    os.remove("/tmp/n"+str(counter))
-    return "bombidibom"
+    f.save('/tmp/n/'+str(counter))
+    return "top"
 
 @app.route("/addFrame", methods=['POST'])
 def addFrame():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/o'+str(counter))
-    mc.fput_object("frames", str(counter), "/tmp/o"+str(counter), "text/plain")
-    os.remove("/tmp/o"+str(counter))
-    return "works"
+    f.save('/tmp/o/'+str(counter))
+    return "top"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2438, debug=True)
