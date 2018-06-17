01292005ecfea11a448a007f0d20fcf4b687a16c
StoneInc <levyehrstein@googlemail.com>
2018-06-17 10:19:51 +0200

    Fixed db clearing

--- DB_clearer.py
+++ DB_clearer.py
@@ -1,4 +1,5 @@
 # Clear frames bucket
+import subprocess
 for _id in range(0, 1000):
     cmd = ['wget', "127.0.0.1:2438/getFrame?id=" + str(_id), "-O", "/tmp/next_frame.dat", "wb+"]
     prc = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input="")
