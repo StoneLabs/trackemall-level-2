3b5e2471b458c7c52017b5fc3491182e9f02b99a
PeterHeynmoeller <peddyheynmoeller@gmail.com>
2018-06-18 23:53:55 +0200

    i don't fucking know why this doesn't work

--- app.py
+++ app.py
@@ -10,27 +10,30 @@ CORS(app)
 
 @app.route("/getDetection", methods=['GET'])
 def getDetection():
-    dirc = os.listdir("/tmp/n/")
+    dirc = os.listdir("/tmp/trackemall/predictions/")
     if(len(dirc)):
         id = + str(request.args.get("id"))
-        f = open("/tmp/n/" + id, "r")
-        #os.remove("/tmp/n/" + id) ONLY FOR LIVE
-        return f
+        try:
+            f = open("/tmp/trackemall/predictions/" + id, "r")
+            #os.remove("/tmp/n/" + id) ONLY FOR LIVE USAGE
+            return f
+        except: #if there aren't enough frames rendered yet
+            return "-1"
     return "-1"
 
 @app.route("/getFrame", methods=['GET'])
 def getFrame():
-    dirc = os.listdir("/tmp/o/")
+    dirc = os.listdir("/tmp/trackemll/frames/")
     if(len(dirc)):
         id = str(request.args.get("id"))
-        f = open("/tmp/o/" + id, "r")
-        os.remove("/tmp/o/" + id)
+        f = open("/tmp/trackemall/frames/" + id, "r")
+        os.remove("/tmp/trackemall/frames/" + id)
         return f
     return "-1"
 
 @app.route("/getNext", methods=['GET'])
 def getNext():
-    dirc = os.listdir("/tmp/o/")
+    dirc = os.listdir("/tmp/trackemall/frames/")
     if(len(dirc)):
         return str(min(dirc))
     return "-1"
@@ -39,20 +42,23 @@ def getNext():
 def setDetection():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/n/'+ str(counter))
+    f.save('/tmp/trackemall/predictions/'+ str(counter))
     return "top"
 
 @app.route("/addFrame", methods=['POST'])
 def addFrame():
     f = request.files['file']
     counter = request.args.get("id")
-    f.save('/tmp/o/'+ str(counter))
+    x = open("/tmp/trackemall/frames/"+str(counter), "w")
+    x.write("")
+    x.close()
+    f.save('/tmp/trackemall/frames/'+str(counter))
     return "top"
 
 if __name__ == '__main__':
     try:
-        os.mkdir("/tmp/n/", 777)
-        os.mkdir("/tmp/o/", 777)
+        os.mkdir("/tmp/trackemall/frames/", 777)
+        os.mkdir("/tmp/trackemall/predictions/", 777)
     except:
-        print("already there")
+        print("you're either smart or pretty fucked..")
     app.run(host='0.0.0.0', port=2438, debug=True)
